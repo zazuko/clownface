ff41e3a690e37a559d3cc711d91c1b94a4f27013
bergi <bergi@axolotlfarm.org>
2018-10-31 18:24:30 +0100

    added .addList method

--- test/Dataset.js
+++ test/Dataset.js
@@ -6,6 +6,12 @@ const rdf = require('rdf-ext')
 const initExample = require('./support/example')
 const Dataset = require('../lib/Dataset')
 
+const ns = {
+  first: rdf.namedNode('http://www.w3.org/1999/02/22-rdf-syntax-ns#first'),
+  rest: rdf.namedNode('http://www.w3.org/1999/02/22-rdf-syntax-ns#rest'),
+  nil: rdf.namedNode('http://www.w3.org/1999/02/22-rdf-syntax-ns#nil')
+}
+
 describe('Dataset', () => {
   let graph
 
@@ -880,6 +886,73 @@ describe('Dataset', () => {
       const cf = clownface.dataset(localGraph, subject)
 
       assert.strictEqual(cf.addOut(predicate, object), cf)
+    })
+  })
+
+  describe('.addList', () => {
+    it('should be a function', () => {
+      const cf = clownface.dataset(graph)
+
+      assert.strictEqual(typeof cf.addList, 'function')
+    })
+
+    it('should throw an error if object parameter is missing', () => {
+      const localGraph = rdf.dataset()
+      const subject = rdf.namedNode('http://localhost:8080/data/person/mary-cooper')
+      const predicate = rdf.namedNode('http://schema.org/knows')
+      const cf = clownface.dataset(localGraph, subject)
+
+      let touched = false
+
+      try {
+        cf.addList(predicate)
+      } catch (err) {
+        touched = true
+      }
+
+      assert(touched)
+    })
+
+    it('should throw an error if predicate parameter is missing', () => {
+      const localGraph = rdf.dataset()
+      const subject = rdf.namedNode('http://localhost:8080/data/person/mary-cooper')
+      const object = rdf.namedNode('http://localhost:8080/data/person/bernadette-rostenkowski')
+      const cf = clownface.dataset(localGraph, subject)
+
+      let touched = false
+
+      try {
+        cf.addList(null, object)
+      } catch (err) {
+        touched = true
+      }
+
+      assert(touched)
+    })
+
+    it('should add list quads using the context term as subject and the given predicate and items', () => {
+      const localGraph = rdf.dataset()
+      const subject = rdf.namedNode('http://localhost:8080/data/person/mary-cooper')
+      const predicate = rdf.namedNode('http://schema.org/counts')
+      const item0 = rdf.literal('0')
+      const item1 = rdf.literal('1')
+      const cf = clownface.dataset(localGraph, subject)
+
+      cf.addList(predicate, [item0, item1])
+
+      const entry = localGraph.match(subject, predicate).toArray()[0]
+      const first0 = localGraph.match(entry.object, ns.first, item0).toArray()[0]
+      const rest0 = localGraph.match(entry.object, ns.rest).toArray()[0]
+      const first1 = localGraph.match(rest0.object, ns.first, item1).toArray()[0]
+      const rest1 = localGraph.match(rest0.object, ns.rest, ns.nil).toArray()[0]
+
+      assert(entry)
+      assert.strictEqual(entry.object.termType, 'BlankNode')
+      assert(first0)
+      assert(rest0)
+      assert.strictEqual(rest0.object.termType, 'BlankNode')
+      assert(first1)
+      assert(rest1)
     })
   })
 
