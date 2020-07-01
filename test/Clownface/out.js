0a9610e97e8ca9357174e07042813b12b7cb1855
tpluscode <tpluscode@users.noreply.github.com>
2020-07-01 08:26:23 +0200

    remove comments in unit tests

--- test/Clownface/out.js
+++ test/Clownface/out.js
@@ -102,147 +102,108 @@ describe('.out', () => {
 
     describe('filtered by single language parameter', () => {
       it('should not return non-literals and non-string literals when language parameter is defined', async () => {
-        // given
         const apple = (await parse(testData)).node(ex.noLabels)
 
-        // when
         const label = apple.out(rdfs.label, { language: '' })
 
-        // then
         assert.strictEqual(label.terms.length, 0)
       })
 
       it('should return exact match for given language', async () => {
-        // given
         const apple = (await parse(testData)).node(ex.apple)
 
-        // when
         const label = apple.out(rdfs.label, { language: 'de' })
 
-        // then
         assert(label.term.equals(rdf.literal('Apfel', 'de')))
       })
 
       it('should return plain string when language is empty string', async () => {
-        // given
         const apple = (await parse(testData)).node(ex.ananas)
 
-        // when
         const label = apple.out(rdfs.label, { language: '' })
 
-        // then
         assert(label.term.equals(rdf.literal('Pineapple')))
       })
 
       it('should skip pointers which do not have matching language', async () => {
-        // given
         const apple = (await parse(testData)).node(ex.ananas)
 
-        // when
         const label = apple.out(rdfs.label, { language: 'en' })
 
-        // then
         assert.strictEqual(label.values.length, 0)
       })
 
       it('should return any result for wildcard language', async () => {
-        // given
         const apple = (await parse(testData)).node(ex.ananas)
 
-        // when
         const label = apple.out(rdfs.label, { language: '*' })
 
-        // then
         assert.ok(label.term)
       })
 
       it('should return all matching literals for a language', async () => {
-        // given
         const apple = (await parse(testData)).node(ex.eggplant)
 
-        // when
         const label = apple.out(rdfs.label, { language: 'pl' })
 
-        // then
         assert.strictEqual(label.terms.length, 3)
       })
 
       it('should return all matching literals for a wildcard language', async () => {
-        // given
         const apple = (await parse(testData)).node(ex.eggplant)
 
-        // when
         const label = apple.out(rdfs.label, { language: '*' })
 
-        // then
         assert.strictEqual(label.terms.length, 3)
       })
 
       it('should be case-insensitive', async () => {
-        // given
         const apple = (await parse(testData)).node(ex.apple)
 
-        // when
         const label = apple.out(rdfs.label, { language: 'SR-cyrl' })
 
-        // then
         assert(label.term.equals(rdf.literal('Јабука', 'sr-Cyrl')))
       })
 
       it('should match secondary language tag by primary', async () => {
-        // given
         const apple = (await parse(testData)).node(ex.apple)
 
-        // when
         const label = apple.out(rdfs.label, { language: 'sr' })
 
-        // then
         assert(label.term.equals(rdf.literal('Јабука', 'sr-Cyrl')))
       })
 
       it('should match secondary language tag by primary regardless of case', async () => {
-        // given
         const apple = (await parse(testData)).node(ex.apple)
 
-        // when
         const label = apple.out(rdfs.label, { language: 'SR' })
 
-        // then
         assert(label.term.equals(rdf.literal('Јабука', 'sr-Cyrl')))
       })
 
       it('should match tertiary tag by secondary language', async () => {
-        // given
         const apple = (await parse(testData)).node(ex.kongressstrasse)
 
-        // when
         const label = apple.out(rdfs.label, { language: 'de-DE' })
 
-        // then
         assert(label.term.equals(rdf.literal('Kongreßstraße', 'de-DE-1901')))
       })
     })
 
     describe('filtered by multiple languages', () => {
       it('should choose first match', async () => {
-        // given
         const apple = (await parse(testData)).node(ex.apple)
 
-        // when
         const label = apple.out(rdfs.label, { language: ['fr', 'no', 'be', 'en', 'de'] })
 
-        // then
         assert(label.term.equals(rdf.literal('Apple', 'en')))
       })
 
       it('should choose exact match over secondary language', async () => {
-        // given
         const apple = (await parse(testData)).node(ex.carrot)
 
-        // when
         const label = apple.out(rdfs.label, { language: ['de-1901', 'de'] })
 
-        // then
         assert(label.term.equals(rdf.literal('Karotte', 'de')))
       })
     })
