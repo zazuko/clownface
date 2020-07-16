facfe6f99c460e6ec3bacb189dac8bf6e24a4c2d
tpluscode <tpluscode@users.noreply.github.com>
2020-07-16 16:21:12 +0200

    docs: correct JSDoc of factory params

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const Clownface = require('./lib/Clownface')
  * @param {Term|Term[]} [init.term] one or more RDF/JS term(s) which will be the pointer's context
  * @param {string} [init.value] one or more raw values which will create literal node as the pointer's context
  * @param {DataFactory} [init.factory=@rdfjs/data-model] an RDF/JS factory which will be used to create nodes
- * @param {Context} [init._context] an existing clownface context. takes precedence before other params
+ * @param {Context|Context[]} [init._context] an existing clownface context. takes precedence before other params
  * @returns {Clownface}
  */
 function factory ({ dataset, graph, term, value, factory, _context }) {
