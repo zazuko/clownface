59e80d4d56e4f2ce52125e424264ca108f847d71
tpluscode <tpluscode@users.noreply.github.com>
2020-08-06 09:18:40 +0200

    fix: mixing clownface version breaks on language

---
+++ test/Context.js
@@ -0,0 +1,17 @@
+const { describe, it } = require('mocha')
+const assert = require('assert')
+const { schema } = require('@tpluscode/rdf-ns-builders')
+const Context = require('../lib/Context')
+const rdf = require('./support/factory')
+
+describe('Context', () => {
+  describe('out', () => {
+    it('can be called without language', () => {
+      const context = new Context({ dataset: rdf.dataset() })
+
+      assert.doesNotThrow(() => {
+        context.out(schema.knows)
+      })
+    })
+  })
+})
