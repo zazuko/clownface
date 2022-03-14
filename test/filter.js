49756a84001c61d656acd71246315f485f5130ce
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2022-03-14 15:50:08 +0100

    feat: filtering pointer by language tag

---
+++ test/filter.js
@@ -0,0 +1,59 @@
+/* global describe, it */
+
+const assert = require('assert')
+const clownface = require('..')
+const { taggedLiteral } = require('../filter')
+const rdf = require('./support/factory')
+const TermSet = require('@rdfjs/term-set')
+
+describe('clownface/filter', () => {
+  describe('.taggedLiteral', () => {
+    it('should return literals for language', async () => {
+      const schemaName = rdf.namedNode('http://schema.org/givenName')
+      const cf = clownface({ dataset: rdf.dataset() })
+        .blankNode()
+        .addOut(schemaName, [
+          rdf.literal('Zurich', 'en'),
+          rdf.literal('Zürich', 'de'),
+          rdf.literal('Zurych', 'pl')])
+        .out(schemaName)
+
+      const result = cf.filter(taggedLiteral(['de']))
+
+      assert(result.term.equals(rdf.literal('Zürich', 'de')))
+    })
+
+    it('should work with multi-pointer', async () => {
+      const containsPlace = rdf.namedNode('http://schema.org/containsPlace')
+      const name = rdf.namedNode('http://schema.org/name')
+      const cf = clownface({ dataset: rdf.dataset() })
+      cf.namedNode('Europe').addOut(containsPlace, [
+        cf.namedNode('CH')
+          .addOut(name, cf.literal('Switzerland', 'en'))
+          .addOut(name, cf.literal('Die Schweiz', 'de')),
+        cf.namedNode('PL')
+          .addOut(name, cf.literal('Poland', 'en'))
+          .addOut(name, cf.literal('Polen', 'de'))
+      ])
+
+      const multi = cf.node([rdf.namedNode('CH'), rdf.namedNode('PL')]).out(name)
+      const result = multi.filter(taggedLiteral(['de']))
+
+      const terms = new TermSet(result.terms)
+      assert.strictEqual(result.terms.length, 2)
+      assert(terms.has(rdf.literal('Die Schweiz', 'de')))
+      assert(terms.has(rdf.literal('Polen', 'de')))
+    })
+
+    it('should ignore non-literals', async () => {
+      const schemaKnows = rdf.namedNode('http://schema.org/knows')
+      const cf = clownface({ dataset: rdf.dataset() })
+        .blankNode()
+        .addOut(schemaKnows, [rdf.blankNode(), rdf.blankNode()])
+
+      const result = cf.out(schemaKnows).filter(taggedLiteral(['de']))
+
+      assert.strictEqual(result.terms.length, 0)
+    })
+  })
+})
