c0c63e684f31ad0f0b8c1513cf0e60b717d35bf5
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2023-07-19 16:16:42 +0200

    test: factory

---
+++ test/Factory.test.js
@@ -0,0 +1,42 @@
+import { expect } from 'chai'
+import Environment from '@rdfjs/environment'
+import DatasetFactory from '@rdfjs/environment/DatasetFactory.js'
+import $rdf from 'rdf-ext'
+import NamespaceFactory from '@rdfjs/environment/NamespaceFactory.js'
+import DataFactory from '@rdfjs/environment/DataFactory.js'
+import ClownfaceFactory from '../Factory.js'
+
+describe('Factory', () => {
+  const env = new Environment([
+    ClownfaceFactory,
+    NamespaceFactory,
+    DatasetFactory,
+    DataFactory,
+  ])
+
+  it('creates dataset using the available factory', () => {
+    // when
+    const ptr = env.clownface()
+
+    // then
+    expect(ptr.dataset).to.be.ok
+  })
+
+  it('uses provided dataset', () => {
+    // when
+    const dataset = $rdf.dataset()
+    const ptr = env.clownface({ dataset })
+
+    // then
+    expect(ptr.dataset).to.eq(dataset)
+  })
+
+  it('forwards arguments', () => {
+    // when
+    const term = $rdf.namedNode('http://example.com')
+    const ptr = env.clownface({ term })
+
+    // then
+    expect(ptr.term).to.deep.eq(term)
+  })
+})
